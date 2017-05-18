c1fd8313926226e968a2bca4ab733918f11d155f
neiljphilip <nphilip33@yahoo.com>
2017-05-17 22:54:25 -0400

    about to begin web service call

--- crio-ios/ViewController.swift
+++ crio-ios/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  crio-ios
 //
-//  Created by Neil Philip on 1/23/17.
+//  Created by Neil Philip for CRIO.
 //  Copyright © 2017 Home. All rights reserved.
 //
 
@@ -13,6 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet var username: UITextField!
     
     @IBOutlet var password: UITextField!
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    @IBAction func register(_ sender: AnyObject) {
+        
+        if username.text == "" || password.text == ""
+        {
+            let alert = UIAlertController(title: "Error in form",
+                                          message: "Empty username/password. Please provide both username and password.",
+                                          preferredStyle: .alert)
+            
+            alert.addAction((UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            
+                self.dismiss(animated: true, completion: nil)
+                
+            
+            })))
+        }
+        else{
+            
+            activityIndicator = UIActivityIndicatorView(frame: CGRect(x:0,y:0,width:50, height: 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            
+        }
+    }
+    
+    @IBAction func logIn(_ sender: AnyObject) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
